ef19188d44e2677ce0bdf3be8d26f58bfbfd4585
Arquila Silva <ahquilasilva0427@gmail.com>
2026-01-13 15:50:23 -0300

    pyautogui

---
+++ codigo.py
@@ -0,0 +1,20 @@
+#Bobliotecas = pacotes de código
+# pip install pyautogui 
+# Passo a passo
+# 1° Passo: Entrar no sistema da empresa
+# 2° Passo: Fazerv login
+# 3° Passo: Abrir a base de dados
+# 4° Passo cadastrar um produto
+# 5° Passo: Repetir o passo 4 até acabar a lista de produtos
+
+import pyautogui
+
+pyautogui.click
+
+
+
+
+
+
+
+
